83f51397697bc92752418b26e6625e5254d0ea10
Sebastien Binet <binet@cern.ch>
2018-06-05 18:15:05 +0200

    zmq4: add subscription test+check to pub/sub

--- zmq4_pubsub_test.go
+++ zmq4_pubsub_test.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"reflect"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -23,15 +24,17 @@ var (
 			name:     "tcp-pub-sub",
 			endpoint: must(EndPoint("tcp")),
 			pub:      zmq4.NewPub(bkg),
-			sub1:     zmq4.NewSub(bkg),
-			sub2:     zmq4.NewSub(bkg),
+			sub0:     zmq4.NewSub(bkg, zmq4.WithID(zmq4.SocketIdentity("sub0"))),
+			sub1:     zmq4.NewSub(bkg, zmq4.WithID(zmq4.SocketIdentity("sub1"))),
+			sub2:     zmq4.NewSub(bkg, zmq4.WithID(zmq4.SocketIdentity("sub2"))),
 		},
 		{
 			name:     "ipc-pub-sub",
 			endpoint: "ipc://ipc-pub-sub",
 			pub:      zmq4.NewPub(bkg),
-			sub1:     zmq4.NewSub(bkg),
-			sub2:     zmq4.NewSub(bkg),
+			sub0:     zmq4.NewSub(bkg, zmq4.WithID(zmq4.SocketIdentity("sub0"))),
+			sub1:     zmq4.NewSub(bkg, zmq4.WithID(zmq4.SocketIdentity("sub1"))),
+			sub2:     zmq4.NewSub(bkg, zmq4.WithID(zmq4.SocketIdentity("sub2"))),
 		},
 	}
 )
@@ -41,16 +44,24 @@ type testCasePubSub struct {
 	skip     bool
 	endpoint string
 	pub      zmq4.Socket
+	sub0     zmq4.Socket
 	sub1     zmq4.Socket
 	sub2     zmq4.Socket
 }
 
 func TestPubSub(t *testing.T) {
 	var (
-		topic = "MSG"
-		msg1  = zmq4.NewMsgString("MSG 1")
-		msg2  = zmq4.NewMsgString("MSG 2")
-		msgs  = []zmq4.Msg{msg1, msg2}
+		//topics = []string{"", "MSG ", "MSG "}
+		topics      = []string{"", "MSG", "msg"}
+		wantNumMsgs = []int{3, 1, 1}
+		msg0        = zmq4.NewMsgString("anything")
+		msg1        = zmq4.NewMsgString("MSG 1")
+		msg2        = zmq4.NewMsgString("msg 2")
+		msgs        = [][]zmq4.Msg{
+			0: {msg0, msg1, msg2},
+			1: {msg1},
+			2: {msg2},
+		}
 	)
 
 	for _, tc := range pubsubs {
@@ -70,10 +81,18 @@ func TestPubSub(t *testing.T) {
 			defer timeout()
 
 			defer tc.pub.Close()
+			defer tc.sub0.Close()
 			defer tc.sub1.Close()
 			defer tc.sub2.Close()
 
-			ready := make(chan int)
+			nmsgs := []int{0, 0, 0}
+			subs := []zmq4.Socket{tc.sub0, tc.sub1, tc.sub2}
+
+			var wg1 sync.WaitGroup
+			var wg2 sync.WaitGroup
+			wg1.Add(len(subs))
+			wg2.Add(len(subs))
+
 			grp, ctx := errgroup.WithContext(ctx)
 			grp.Go(func() error {
 
@@ -82,12 +101,12 @@ func TestPubSub(t *testing.T) {
 					return errors.Wrapf(err, "could not listen")
 				}
 
-				<-ready
-				<-ready
+				wg1.Wait()
+				wg2.Wait()
 
 				time.Sleep(1 * time.Second)
 
-				for _, msg := range msgs {
+				for _, msg := range msgs[0] {
 					err = tc.pub.Send(msg)
 					if err != nil {
 						return errors.Wrapf(err, "could not send message %v", msg)
@@ -97,7 +116,7 @@ func TestPubSub(t *testing.T) {
 				return err
 			})
 
-			for isub, sub := range []zmq4.Socket{tc.sub1, tc.sub2} {
+			for isub := range subs {
 				func(isub int, sub zmq4.Socket) {
 					grp.Go(func() error {
 						var err error
@@ -105,29 +124,42 @@ func TestPubSub(t *testing.T) {
 						if err != nil {
 							return errors.Wrapf(err, "could not dial")
 						}
-						err = sub.SetOption(zmq4.OptionSubscribe, topic)
+						wg1.Done()
+						wg1.Wait()
+
+						err = sub.SetOption(zmq4.OptionSubscribe, topics[isub])
 						if err != nil {
-							return errors.Wrapf(err, "could not subscribe to topic %q", topic)
+							return errors.Wrapf(err, "could not subscribe to topic %q", topics[isub])
 						}
 
-						ready <- isub
-						for _, want := range msgs {
+						wg2.Done()
+						wg2.Wait()
+
+						msgs := msgs[isub]
+						for imsg, want := range msgs {
 							msg, err := sub.Recv()
 							if err != nil {
 								return errors.Wrapf(err, "could not recv message %v", want)
 							}
 							if !reflect.DeepEqual(msg, want) {
-								return errors.Errorf("got = %v, want= %v", msg, want)
+								return errors.Errorf("sub[%d][msg=%d]: got = %v, want= %v", isub, imsg, msg, want)
 							}
+							nmsgs[isub]++
 						}
 
 						return err
 					})
-				}(isub, sub)
+				}(isub, subs[isub])
 			}
 
 			if err := grp.Wait(); err != nil {
 				t.Fatal(err)
+			}
+
+			for i, want := range wantNumMsgs {
+				if want != nmsgs[i] {
+					t.Errorf("sub[%d]: got %d messages, want %d msgs=%v", i, nmsgs[i], want, nmsgs)
+				}
 			}
 		})
 	}
