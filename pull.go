3ed2599e4c4c8a4bb8ce2f72bfdcd2e9f19d4392
Sebastien Binet <binet@cern.ch>
2018-06-05 14:32:45 +0200

    zmq4: unembed socket from pullSocket

--- pull.go
+++ pull.go
@@ -18,13 +18,48 @@ func NewPull(ctx context.Context, opts ...Option) Socket {
 
 // pullSocket is a PULL ZeroMQ socket.
 type pullSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (pull *pullSocket) Close() error {
+	return pull.sck.Close()
 }
 
 // Send puts the message on the outbound send queue.
 // Send blocks until the message can be queued or the send deadline expires.
 func (*pullSocket) Send(msg Msg) error {
 	return errors.Errorf("zmq4: PULL sockets can't send messages")
+}
+
+// Recv receives a complete message.
+func (pull *pullSocket) Recv() (Msg, error) {
+	return pull.sck.Recv()
+}
+
+// Listen connects a local endpoint to the Socket.
+func (pull *pullSocket) Listen(ep string) error {
+	return pull.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (pull *pullSocket) Dial(ep string) error {
+	return pull.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (pull *pullSocket) Type() SocketType {
+	return pull.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (pull *pullSocket) GetOption(name string) (interface{}, error) {
+	return pull.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (pull *pullSocket) SetOption(name string, value interface{}) error {
+	return pull.sck.SetOption(name, value)
 }
 
 var (
