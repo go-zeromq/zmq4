4e6051d9acfbe4464c47fb8973b5abc7eb37b698
Sebastien Binet <binet@cern.ch>
2018-06-05 14:42:54 +0200

    zmq4: unembed socket from xpubSocket

--- xpub.go
+++ xpub.go
@@ -11,12 +11,54 @@ import (
 // NewXPub returns a new XPUB ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewXPub(ctx context.Context, opts ...Option) Socket {
-	return &xpubSocket{newSocket(ctx, XPub, opts...)}
+	xpub := &xpubSocket{newSocket(ctx, XPub, opts...)}
+	return xpub
 }
 
 // xpubSocket is a XPUB ZeroMQ socket.
 type xpubSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (xpub *xpubSocket) Close() error {
+	return xpub.sck.Close()
+}
+
+// Send puts the message on the outbound send queue.
+// Send blocks until the message can be queued or the send deadline expires.
+func (xpub *xpubSocket) Send(msg Msg) error {
+	return xpub.sck.Send(msg)
+}
+
+// Recv receives a complete message.
+func (xpub *xpubSocket) Recv() (Msg, error) {
+	return xpub.sck.Recv()
+}
+
+// Listen connects a local endpoint to the Socket.
+func (xpub *xpubSocket) Listen(ep string) error {
+	return xpub.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (xpub *xpubSocket) Dial(ep string) error {
+	return xpub.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (xpub *xpubSocket) Type() SocketType {
+	return xpub.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (xpub *xpubSocket) GetOption(name string) (interface{}, error) {
+	return xpub.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (xpub *xpubSocket) SetOption(name string, value interface{}) error {
+	return xpub.sck.SetOption(name, value)
 }
 
 var (
