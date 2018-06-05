c3aec67b9f0c0b9f0009f8277f61f8e4d83a61ee
Sebastien Binet <binet@cern.ch>
2018-06-05 14:41:16 +0200

    zmq4: unembed socket from pubSocket

--- pub.go
+++ pub.go
@@ -6,34 +6,66 @@ package zmq4
 
 import (
 	"context"
+
+	"github.com/pkg/errors"
 )
 
 // NewPub returns a new PUB ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewPub(ctx context.Context, opts ...Option) Socket {
-	return &pubSocket{newSocket(ctx, Pub, opts...)}
+	pub := &pubSocket{sck: newSocket(ctx, Pub, opts...)}
+	pub.sck.w = newMWriter()
+	return pub
 }
 
 // pubSocket is a PUB ZeroMQ socket.
 type pubSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (pub *pubSocket) Close() error {
+	return pub.sck.Close()
 }
 
 // Send puts the message on the outbound send queue.
 // Send blocks until the message can be queued or the send deadline expires.
 func (pub *pubSocket) Send(msg Msg) error {
-	pub.socket.isReady()
-	pub.socket.mu.RLock()
-	var err error
 	// FIXME(sbinet): only send to correct subscribers...
-	for _, conn := range pub.conns {
-		e := conn.SendMsg(msg)
-		if e != nil && err == nil {
-			err = e
-		}
-	}
-	pub.socket.mu.RUnlock()
-	return err
+	ctx, cancel := context.WithTimeout(pub.sck.ctx, pub.sck.timeout())
+	defer cancel()
+	return pub.sck.w.write(ctx, msg)
+}
+
+// Recv receives a complete message.
+func (*pubSocket) Recv() (Msg, error) {
+	msg := Msg{err: errors.Errorf("zmq4: PUB sockets can't recv messages")}
+	return msg, msg.err
+}
+
+// Listen connects a local endpoint to the Socket.
+func (pub *pubSocket) Listen(ep string) error {
+	return pub.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (pub *pubSocket) Dial(ep string) error {
+	return pub.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (pub *pubSocket) Type() SocketType {
+	return pub.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (pub *pubSocket) GetOption(name string) (interface{}, error) {
+	return pub.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (pub *pubSocket) SetOption(name string, value interface{}) error {
+	return pub.sck.SetOption(name, value)
 }
 
 var (
