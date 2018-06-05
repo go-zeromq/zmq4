6fd98c66a1c921f44813a90e500777f1441035fb
Sebastien Binet <binet@cern.ch>
2018-06-05 14:29:45 +0200

    zmq4: unembed socket from dealerSocket

--- dealer.go
+++ dealer.go
@@ -11,12 +11,54 @@ import (
 // NewDealer returns a new DEALER ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewDealer(ctx context.Context, opts ...Option) Socket {
-	return &dealerSocket{newSocket(ctx, Dealer, opts...)}
+	dealer := &dealerSocket{newSocket(ctx, Dealer, opts...)}
+	return dealer
 }
 
 // dealerSocket is a DEALER ZeroMQ socket.
 type dealerSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (dealer *dealerSocket) Close() error {
+	return dealer.sck.Close()
+}
+
+// Send puts the message on the outbound send queue.
+// Send blocks until the message can be queued or the send deadline expires.
+func (dealer *dealerSocket) Send(msg Msg) error {
+	return dealer.sck.Send(msg)
+}
+
+// Recv receives a complete message.
+func (dealer *dealerSocket) Recv() (Msg, error) {
+	return dealer.sck.Recv()
+}
+
+// Listen connects a local endpoint to the Socket.
+func (dealer *dealerSocket) Listen(ep string) error {
+	return dealer.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (dealer *dealerSocket) Dial(ep string) error {
+	return dealer.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (dealer *dealerSocket) Type() SocketType {
+	return dealer.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (dealer *dealerSocket) GetOption(name string) (interface{}, error) {
+	return dealer.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (dealer *dealerSocket) SetOption(name string, value interface{}) error {
+	return dealer.sck.SetOption(name, value)
 }
 
 var (
