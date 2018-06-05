5e7111ad81179627e6f55cba0a541735bfad08cd
Sebastien Binet <binet@cern.ch>
2018-06-05 14:37:06 +0200

    zmq4: unembed socket from repSocket

--- push.go
+++ push.go
@@ -13,17 +13,55 @@ import (
 // NewPush returns a new PUSH ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewPush(ctx context.Context, opts ...Option) Socket {
-	return &pushSocket{newSocket(ctx, Push, opts...)}
+	push := &pushSocket{newSocket(ctx, Push, opts...)}
+	push.sck.r = nil
+	return push
 }
 
 // pushSocket is a PUSH ZeroMQ socket.
 type pushSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (push *pushSocket) Close() error {
+	return push.sck.Close()
+}
+
+// Send puts the message on the outbound send queue.
+// Send blocks until the message can be queued or the send deadline expires.
+func (push *pushSocket) Send(msg Msg) error {
+	return push.sck.Send(msg)
 }
 
 // Recv receives a complete message.
 func (*pushSocket) Recv() (Msg, error) {
 	return Msg{}, errors.Errorf("zmq4: PUSH sockets can't recv messages")
+}
+
+// Listen connects a local endpoint to the Socket.
+func (push *pushSocket) Listen(ep string) error {
+	return push.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (push *pushSocket) Dial(ep string) error {
+	return push.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (push *pushSocket) Type() SocketType {
+	return push.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (push *pushSocket) GetOption(name string) (interface{}, error) {
+	return push.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (push *pushSocket) SetOption(name string, value interface{}) error {
+	return push.sck.SetOption(name, value)
 }
 
 var (
--- rep.go
+++ rep.go
@@ -11,28 +11,59 @@ import (
 // NewRep returns a new REP ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewRep(ctx context.Context, opts ...Option) Socket {
-	return &repSocket{newSocket(ctx, Rep, opts...)}
+	rep := &repSocket{newSocket(ctx, Rep, opts...)}
+	return rep
 }
 
 // repSocket is a REP ZeroMQ socket.
 type repSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (rep *repSocket) Close() error {
+	return rep.sck.Close()
 }
 
 // Send puts the message on the outbound send queue.
 // Send blocks until the message can be queued or the send deadline expires.
 func (rep *repSocket) Send(msg Msg) error {
 	msg.Frames = append([][]byte{nil}, msg.Frames...)
-	return rep.socket.Send(msg)
+	return rep.sck.Send(msg)
 }
 
 // Recv receives a complete message.
 func (rep *repSocket) Recv() (Msg, error) {
-	msg, err := rep.socket.Recv()
+	msg, err := rep.sck.Recv()
 	if len(msg.Frames) > 1 {
 		msg.Frames = msg.Frames[1:]
 	}
 	return msg, err
+}
+
+// Listen connects a local endpoint to the Socket.
+func (rep *repSocket) Listen(ep string) error {
+	return rep.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (rep *repSocket) Dial(ep string) error {
+	return rep.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (rep *repSocket) Type() SocketType {
+	return rep.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (rep *repSocket) GetOption(name string) (interface{}, error) {
+	return rep.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (rep *repSocket) SetOption(name string, value interface{}) error {
+	return rep.sck.SetOption(name, value)
 }
 
 var (
