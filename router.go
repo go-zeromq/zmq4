5c2b41ae36e95ec4b0a8901f1882d6cef96b52d8
Sebastien Binet <binet@cern.ch>
2018-06-05 14:39:29 +0200

    zmq4: unembed socket from routerSocket

--- router.go
+++ router.go
@@ -11,12 +11,54 @@ import (
 // NewRouter returns a new ROUTER ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewRouter(ctx context.Context, opts ...Option) Socket {
-	return &routerSocket{newSocket(ctx, Router, opts...)}
+	router := &routerSocket{newSocket(ctx, Router, opts...)}
+	return router
 }
 
 // routerSocket is a ROUTER ZeroMQ socket.
 type routerSocket struct {
-	*socket
+	sck *socket
+}
+
+// Close closes the open Socket
+func (router *routerSocket) Close() error {
+	return router.sck.Close()
+}
+
+// Send puts the message on the outbound send queue.
+// Send blocks until the message can be queued or the send deadline expires.
+func (router *routerSocket) Send(msg Msg) error {
+	return router.sck.Send(msg)
+}
+
+// Recv receives a complete message.
+func (router *routerSocket) Recv() (Msg, error) {
+	return router.sck.Recv()
+}
+
+// Listen connects a local endpoint to the Socket.
+func (router *routerSocket) Listen(ep string) error {
+	return router.sck.Listen(ep)
+}
+
+// Dial connects a remote endpoint to the Socket.
+func (router *routerSocket) Dial(ep string) error {
+	return router.sck.Dial(ep)
+}
+
+// Type returns the type of this Socket (PUB, SUB, ...)
+func (router *routerSocket) Type() SocketType {
+	return router.sck.Type()
+}
+
+// GetOption is used to retrieve an option for a socket.
+func (router *routerSocket) GetOption(name string) (interface{}, error) {
+	return router.sck.GetOption(name)
+}
+
+// SetOption is used to set an option for a socket.
+func (router *routerSocket) SetOption(name string, value interface{}) error {
+	return router.sck.SetOption(name, value)
 }
 
 var (
