ab931c62f4efcd5e435750d31ddefebc82ca2e66
Sebastien Binet <binet@cern.ch>
2018-06-13 22:16:54 +0200

    example: add req-rep client/worker example

---
+++ example/rrclient.go
@@ -0,0 +1,43 @@
+// Copyright 2018 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build ignore
+
+// Request-reply client.
+//
+// Connects REQ socket to tcp://localhost:5559
+// Sends "Hello" to server, expects "World" back
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-zeromq/zmq4"
+)
+
+func main() {
+	log.SetPrefix("rrclient: ")
+
+	req := zmq4.NewReq(context.Background())
+	defer req.Close()
+
+	err := req.Dial("tcp://localhost:5559")
+	if err != nil {
+		log.Fatalf("could not dial: %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		err := req.Send(zmq4.NewMsgString("Hello"))
+		if err != nil {
+			log.Fatalf("could not send greeting: %v", err)
+		}
+
+		msg, err := req.Recv()
+		if err != nil {
+			log.Fatalf("could not recv greeting: %v", err)
+		}
+		log.Printf("received reply %d [%s]\n", i, msg.Frames[0])
+	}
+}
---
+++ example/rrworker.go
@@ -0,0 +1,51 @@
+// Copyright 2018 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build ignore
+
+// Request-reply worker.
+//
+// Connects REP socket to tcp://*:5559
+// Expects "Hello" from client, replies with "World"
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/go-zeromq/zmq4"
+)
+
+func main() {
+	log.SetPrefix("rrworker: ")
+
+	//  Socket to talk to clients
+	rep := zmq4.NewRep(context.Background())
+	defer rep.Close()
+
+	err := rep.Listen("tcp://*:5559")
+	if err != nil {
+		log.Fatalf("could not dial: %v", err)
+	}
+
+	for {
+		//  Wait for next request from client
+		msg, err := rep.Recv()
+		if err != nil {
+			log.Fatalf("could not recv request: %v", err)
+		}
+
+		log.Printf("received request: [%s]\n", msg.Frames[0])
+
+		//  Do some 'work'
+		time.Sleep(time.Second)
+
+		//  Send reply back to client
+		err = rep.Send(zmq4.NewMsgString("World"))
+		if err != nil {
+			log.Fatalf("could not send reply: %v", err)
+		}
+	}
+}
