f0c4257f24f03a80b3024b6aefb16bcf976c9129
Sebastien Binet <binet@cern.ch>
2018-06-13 22:03:36 +0200

    example: add pub-sub envelope example

---
+++ example/psenvpub.go
@@ -0,0 +1,50 @@
+// Copyright 2018 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build ignore
+
+// PubSub envelope publisher
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/go-zeromq/zmq4"
+)
+
+func main() {
+	log.SetPrefix("psenvpub: ")
+
+	// prepare the publisher
+	pub := zmq4.NewPub(context.Background())
+	defer pub.Close()
+
+	err := pub.Listen("tcp://*:5563")
+	if err != nil {
+		log.Fatalf("could not listen: %v", err)
+	}
+
+	msgA := zmq4.NewMsgFrom(
+		[]byte("A"),
+		[]byte("We don't want to see this"),
+	)
+	msgB := zmq4.NewMsgFrom(
+		[]byte("B"),
+		[]byte("We would like to see this"),
+	)
+	for {
+		//  Write two messages, each with an envelope and content
+		err = pub.Send(msgA)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = pub.Send(msgB)
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Second)
+	}
+}
---
+++ example/psenvsub.go
@@ -0,0 +1,42 @@
+// Copyright 2018 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build ignore
+
+// PubSub envelope subscriber
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-zeromq/zmq4"
+)
+
+func main() {
+	log.SetPrefix("psenvsub: ")
+
+	//  Prepare our subscriber
+	sub := zmq4.NewSub(context.Background())
+	defer sub.Close()
+
+	err := sub.Dial("tcp://localhost:5563")
+	if err != nil {
+		log.Fatalf("could not dial: %v", err)
+	}
+
+	err = sub.SetOption(zmq4.OptionSubscribe, "B")
+	if err != nil {
+		log.Fatalf("could not subscribe: %v", err)
+	}
+
+	for {
+		// Read envelope
+		msg, err := sub.Recv()
+		if err != nil {
+			log.Fatalf("could not receive message: %v", err)
+		}
+		log.Printf("[%s] %s\n", msg.Frames[0], msg.Frames[1])
+	}
+}
