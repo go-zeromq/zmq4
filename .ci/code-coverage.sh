7a88be9e25e1e2eea79247412f5b15cdde2beb1e
Sebastien Binet <binet@cern.ch>
2020-01-21 17:16:45 +0100

    ci: make test verbose

--- .ci/code-coverage.sh
+++ .ci/code-coverage.sh
@@ -10,7 +10,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test $TAGS -race -coverprofile=profile.out -covermode=atomic $d
+    go test -v $TAGS -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
