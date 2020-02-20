53b789b34aaa3ef983e323fab4db1dce8075e5fd
Murphy Law <mlaw2501@gmail.com>
2020-02-19 22:39:30 -0800

    zmq4: implement proper REQ and REP socket
    This fixes problems with REP sending messages to all connected peers
    rather than to the originating peer.
    
    The REQ/REP semantics now matches the recommended implementation behavior as specified in the ZMQ RFC.
    In particular:
    * REQ shall route outgoing messages to connected peers using a
    round-robin strategy
    * REQ shall block on sending, or return a suitable error, when it has no
    connected peers.
    * REP shall not block on sending
    
    Thus, this changes the behavior a bit as REP no longer blocks on Send.
    
    resolves #70

--- req.go
+++ req.go
@@ -7,18 +7,25 @@ package zmq4
 import (
 	"context"
 	"net"
+	"sync"
+
+	"golang.org/x/xerrors"
 )
 
 // NewReq returns a new REQ ZeroMQ socket.
 // The returned socket value is initially unbound.
 func NewReq(ctx context.Context, opts ...Option) Socket {
-	req := &reqSocket{newSocket(ctx, Req, opts...)}
+	state := &reqState{}
+	req := &reqSocket{newSocket(ctx, Req, opts...), state}
+	req.sck.r = newReqReader(req.sck.ctx, state)
+	req.sck.w = newReqWriter(req.sck.ctx, state)
 	return req
 }
 
 // reqSocket is a REQ ZeroMQ socket.
 type reqSocket struct {
-	sck *socket
+	sck   *socket
+	state *reqState
 }
 
 // Close closes the open Socket
@@ -29,24 +36,27 @@ func (req *reqSocket) Close() error {
 // Send puts the message on the outbound send queue.
 // Send blocks until the message can be queued or the send deadline expires.
 func (req *reqSocket) Send(msg Msg) error {
-	msg.Frames = append([][]byte{nil}, msg.Frames...)
-	return req.sck.Send(msg)
+	ctx, cancel := context.WithTimeout(req.sck.ctx, req.sck.timeout())
+	defer cancel()
+	return req.sck.w.write(ctx, msg)
 }
 
 // SendMulti puts the message on the outbound send queue.
 // SendMulti blocks until the message can be queued or the send deadline expires.
 // The message will be sent as a multipart message.
 func (req *reqSocket) SendMulti(msg Msg) error {
-	msg.Frames = append([][]byte{nil}, msg.Frames...)
-	return req.sck.SendMulti(msg)
+	msg.multipart = true
+	ctx, cancel := context.WithTimeout(req.sck.ctx, req.sck.timeout())
+	defer cancel()
+	return req.sck.w.write(ctx, msg)
 }
 
 // Recv receives a complete message.
 func (req *reqSocket) Recv() (Msg, error) {
-	msg, err := req.sck.Recv()
-	if len(msg.Frames) > 1 {
-		msg.Frames = msg.Frames[1:]
-	}
+	ctx, cancel := context.WithCancel(req.sck.ctx)
+	defer cancel()
+	var msg Msg
+	err := req.sck.r.read(ctx, &msg)
 	return msg, err
 }
 
@@ -79,6 +89,139 @@ func (req *reqSocket) GetOption(name string) (interface{}, error) {
 // SetOption is used to set an option for a socket.
 func (req *reqSocket) SetOption(name string, value interface{}) error {
 	return req.sck.SetOption(name, value)
+}
+
+type reqWriter struct {
+	ctx      context.Context
+	mu       sync.Mutex
+	conns    []*Conn
+	nextConn int
+	state    *reqState
+}
+
+type reqReader struct {
+	ctx   context.Context
+	state *reqState
+}
+
+func newReqWriter(ctx context.Context, state *reqState) *reqWriter {
+	return &reqWriter{
+		ctx:   ctx,
+		state: state,
+	}
+}
+
+func (r *reqWriter) write(ctx context.Context, msg Msg) error {
+	msg.Frames = append([][]byte{nil}, msg.Frames...)
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	var err error
+	for i := 0; i < len(r.conns); i++ {
+		cur := i + r.nextConn%len(r.conns)
+		conn := r.conns[cur]
+		err = conn.SendMsg(msg)
+		if err == nil {
+			r.nextConn = cur + 1%len(r.conns)
+			r.state.Set(conn)
+			return nil
+		}
+	}
+	return xerrors.Errorf("zmq4: no connections available: %w", err)
+}
+
+func (r *reqWriter) addConn(c *Conn) {
+	r.mu.Lock()
+	r.conns = append(r.conns, c)
+	r.mu.Unlock()
+}
+
+func (r *reqWriter) rmConn(conn *Conn) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	cur := -1
+	for i := range r.conns {
+		if r.conns[i] == conn {
+			cur = i
+			break
+		}
+	}
+	if cur >= 0 {
+		r.conns = append(r.conns[:cur], r.conns[cur+1:]...)
+	}
+
+	r.state.Reset(conn)
+}
+
+func (r *reqWriter) Close() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	var err error
+	for _, conn := range r.conns {
+		e := conn.Close()
+		if e != nil && err == nil {
+			err = e
+		}
+	}
+	r.conns = nil
+	return err
+}
+
+func newReqReader(ctx context.Context, state *reqState) *reqReader {
+	return &reqReader{
+		ctx:   ctx,
+		state: state,
+	}
+}
+
+func (r *reqReader) addConn(c *Conn) {}
+func (r *reqReader) rmConn(c *Conn)  {}
+
+func (r *reqReader) Close() error {
+	return nil
+}
+
+func (r *reqReader) read(ctx context.Context, msg *Msg) error {
+	curConn := r.state.Get()
+	if curConn == nil {
+		return xerrors.Errorf("zmq4: no connections available")
+	}
+	*msg = curConn.read()
+	if msg.err != nil {
+		return msg.err
+	}
+	if len(msg.Frames) > 1 {
+		msg.Frames = msg.Frames[1:]
+	}
+	return nil
+}
+
+type reqState struct {
+	lastConn *Conn
+	mu       sync.Mutex
+}
+
+func (r *reqState) Set(conn *Conn) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.lastConn = conn
+}
+
+// Reset resets the state iff c matches the resident connection
+func (r *reqState) Reset(c *Conn) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if r.lastConn == c {
+		r.lastConn = nil
+	}
+}
+
+func (r *reqState) Get() *Conn {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.lastConn
 }
 
 var (
--- zmq4_reqrep_test.go
+++ zmq4_reqrep_test.go
@@ -43,6 +43,7 @@ type testCaseReqRep struct {
 	skip     bool
 	endpoint string
 	req      zmq4.Socket
+	req2     zmq4.Socket
 	rep      zmq4.Socket
 }
 
@@ -143,6 +144,181 @@ func TestReqRep(t *testing.T) {
 					}
 				}
 
+				return err
+			})
+			if err := grp.Wait(); err != nil {
+				t.Fatalf("error: %+v", err)
+			}
+		})
+	}
+}
+
+func TestMultiReqRepIssue70(t *testing.T) {
+	var (
+		reqName  = zmq4.NewMsgString("NAME")
+		reqLang  = zmq4.NewMsgString("LANG")
+		reqQuit  = zmq4.NewMsgString("QUIT")
+		reqName2 = zmq4.NewMsgString("NAME2")
+		reqLang2 = zmq4.NewMsgString("LANG2")
+		reqQuit2 = zmq4.NewMsgString("QUIT2")
+		repName  = zmq4.NewMsgString("zmq4")
+		repLang  = zmq4.NewMsgString("Go")
+		repQuit  = zmq4.NewMsgString("bye")
+		repName2 = zmq4.NewMsgString("zmq42")
+		repLang2 = zmq4.NewMsgString("Go2")
+		repQuit2 = zmq4.NewMsgString("bye2")
+	)
+
+	reqreps := []testCaseReqRep{
+		{
+			name:     "tcp-req-rep",
+			endpoint: must(EndPoint("tcp")),
+			req:      zmq4.NewReq(bkg),
+			req2:     zmq4.NewReq(bkg),
+			rep:      zmq4.NewRep(bkg),
+		},
+		{
+			name:     "ipc-req-rep",
+			endpoint: "ipc://ipc-req-rep",
+			req:      zmq4.NewReq(bkg),
+			req2:     zmq4.NewReq(bkg),
+			rep:      zmq4.NewRep(bkg),
+		},
+		{
+			name:     "inproc-req-rep",
+			endpoint: "inproc://inproc-req-rep",
+			req:      zmq4.NewReq(bkg),
+			req2:     zmq4.NewReq(bkg),
+			rep:      zmq4.NewRep(bkg),
+		},
+	}
+
+	for i := range reqreps {
+		tc := reqreps[i]
+		t.Run(tc.name, func(t *testing.T) {
+			defer tc.req.Close()
+			defer tc.req2.Close()
+			defer tc.rep.Close()
+
+			if tc.skip {
+				t.Skipf(tc.name)
+			}
+			t.Parallel()
+
+			ep := tc.endpoint
+			cleanUp(ep)
+
+			ctx, timeout := context.WithTimeout(context.Background(), 20*time.Second)
+			defer timeout()
+
+			grp, ctx := errgroup.WithContext(ctx)
+			grp.Go(func() error {
+				err := tc.rep.Listen(ep)
+				if err != nil {
+					return xerrors.Errorf("could not listen: %w", err)
+				}
+
+				if addr := tc.rep.Addr(); addr == nil {
+					return xerrors.Errorf("listener with nil Addr")
+				}
+
+				loop1, loop2 := true, true
+				for loop1 || loop2 {
+					msg, err := tc.rep.Recv()
+					if err != nil {
+						return xerrors.Errorf("could not recv REQ message: %w", err)
+					}
+					var rep zmq4.Msg
+					switch string(msg.Frames[0]) {
+					case "NAME":
+						rep = repName
+					case "LANG":
+						rep = repLang
+					case "QUIT":
+						rep = repQuit
+						loop1 = false
+					case "NAME2":
+						rep = repName2
+					case "LANG2":
+						rep = repLang2
+					case "QUIT2":
+						rep = repQuit2
+						loop2 = false
+					}
+
+					err = tc.rep.Send(rep)
+					if err != nil {
+						return xerrors.Errorf("could not send REP message to %v: %w", msg, err)
+					}
+				}
+				return err
+			})
+			grp.Go(func() error {
+
+				err := tc.req2.Dial(ep)
+				if err != nil {
+					return xerrors.Errorf("could not dial: %w", err)
+				}
+
+				if addr := tc.req2.Addr(); addr != nil {
+					return xerrors.Errorf("dialer with non-nil Addr")
+				}
+
+				for _, msg := range []struct {
+					req zmq4.Msg
+					rep zmq4.Msg
+				}{
+					{reqName2, repName2},
+					{reqLang2, repLang2},
+					{reqQuit2, repQuit2},
+				} {
+					err = tc.req2.Send(msg.req)
+					if err != nil {
+						return xerrors.Errorf("could not send REQ message %v: %w", msg.req, err)
+					}
+					rep, err := tc.req2.Recv()
+					if err != nil {
+						return xerrors.Errorf("could not recv REP message %v: %w", msg.req, err)
+					}
+
+					if got, want := rep, msg.rep; !reflect.DeepEqual(got, want) {
+						return xerrors.Errorf("got = %v, want= %v", got, want)
+					}
+				}
+				return err
+			})
+			grp.Go(func() error {
+
+				err := tc.req.Dial(ep)
+				if err != nil {
+					return xerrors.Errorf("could not dial: %w", err)
+				}
+
+				if addr := tc.req.Addr(); addr != nil {
+					return xerrors.Errorf("dialer with non-nil Addr")
+				}
+
+				for _, msg := range []struct {
+					req zmq4.Msg
+					rep zmq4.Msg
+				}{
+					{reqName, repName},
+					{reqLang, repLang},
+					{reqQuit, repQuit},
+				} {
+					err = tc.req.Send(msg.req)
+					if err != nil {
+						return xerrors.Errorf("could not send REQ message %v: %w", msg.req, err)
+					}
+					rep, err := tc.req.Recv()
+					if err != nil {
+						return xerrors.Errorf("could not recv REP message %v: %w", msg.req, err)
+					}
+
+					if got, want := rep, msg.rep; !reflect.DeepEqual(got, want) {
+						return xerrors.Errorf("got = %v, want= %v", got, want)
+					}
+				}
 				return err
 			})
 			if err := grp.Wait(); err != nil {
