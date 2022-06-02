ae3a4cef5c0ff09b926edbf9915ec6dc0ad2dcde
Issac Trotts <15530+ijt@users.noreply.github.com>
2022-06-02 01:10:41 -0600

    example: add hello client/server
    Derived from https://zguide.zeromq.org/docs/chapter1

---
+++ example/hwclient.go
@@ -0,0 +1,51 @@
+// Copyright 2022 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// Hello client
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	zmq "github.com/go-zeromq/zmq4"
+)
+
+func main() {
+	if err := hwclient(); err != nil {
+		log.Fatalf("hwclient: %v", err)
+	}
+}
+
+func hwclient() error {
+	ctx := context.Background()
+	socket := zmq.NewReq(ctx, zmq.WithDialerRetry(time.Second))
+	defer socket.Close()
+
+	fmt.Printf("Connecting to hello world server...")
+	if err := socket.Dial("tcp://localhost:5555"); err != nil {
+		return fmt.Errorf("dialing: %w", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		// Send hello.
+		m := zmq.NewMsgString("hello")
+		fmt.Println("sending ", m)
+		if err := socket.Send(m); err != nil {
+			return fmt.Errorf("sending: %w", err)
+		}
+
+		// Wait for reply.
+		r, err := socket.Recv()
+		if err != nil {
+			return fmt.Errorf("receiving: %w", err)
+		}
+		fmt.Println("received ", r.String())
+	}
+	return nil
+}
---
+++ example/hwserver.go
@@ -0,0 +1,50 @@
+// Copyright 2022 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// Hello server
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	zmq "github.com/go-zeromq/zmq4"
+)
+
+func main() {
+	if err := hwserver(); err != nil {
+		log.Fatalf("hwserver: %w", err)
+	}
+}
+
+func hwserver() error {
+	ctx := context.Background()
+	// Socket to talk to clients
+	socket := zmq.NewRep(ctx)
+	defer socket.Close()
+	if err := socket.Listen("tcp://*:5555"); err != nil {
+		return fmt.Errorf("listening: %w", err)
+	}
+
+	for {
+		msg, err := socket.Recv()
+		if err != nil {
+			return fmt.Errorf("receiving: %w", err)
+		}
+		fmt.Println("Received ", msg)
+
+		// Do some 'work'
+		time.Sleep(time.Second)
+
+		reply := fmt.Sprintf("World")
+		if err := socket.Send(zmq.NewMsgString(reply)); err != nil {
+			return fmt.Errorf("sending reply: %w", err)
+		}
+	}
+}
+
