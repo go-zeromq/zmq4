ae18bc07fdc5564fb4f31a36821cf13339ea3efd
thielepaul <thielepaul@gmail.com>
2022-06-20 10:04:53 +0200

    zmq4: make number of retries for dial configurable

--- options.go
+++ options.go
@@ -51,6 +51,14 @@ func WithLogger(msg *log.Logger) Option {
 	}
 }
 
+// WithDialerMaxRetries configures the maximum number of retries
+// when dialing an endpoint (-1 means infinite retries).
+func WithDialerMaxRetries(maxRetries int) Option {
+	return func(s *socket) {
+		s.maxRetries = maxRetries
+	}
+}
+
 /*
 // TODO(sbinet)
 
